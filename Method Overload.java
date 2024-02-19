15c48c3819356d4a0623da30888c405ebf44fb2e
Ganesh Malepati <97588943+ganeshmalepati@users.noreply.github.com>
2024-02-19 16:58:42 +0530

    Create Method Overload.java

---
+++ Method Overload.java
@@ -0,0 +1,19 @@
+class MethodOverloading{
+    public int add(int n1, int n2, int n3){
+        return n1+n2+n3;
+    }
+    public int add(int n1, int n2){
+        return n1+n2;
+    }
+    public double add(double n1, int n2){
+        return n1+n2;
+    }
+}
+
+public class Moverloading {
+    public static void main(String args[]){
+        MethodOverloading mo=new MethodOverloading();
+        int r=mo.add(9, 8);
+        System.err.println(r);
+    }
+}
